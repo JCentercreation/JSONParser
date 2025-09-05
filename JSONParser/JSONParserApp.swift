641087b9d460517c83d4cd0d277ce4abb962a91b
JCentercreation <jcentercreation@gmail.com>
2025-09-05 20:38:40 +0200

    Initial Commit

---
+++ JSONParser/JSONParserApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONParserApp.swift
+//  JSONParser
+//
+//  Created by Javier Carrillo Gallego on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct JSONParserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
